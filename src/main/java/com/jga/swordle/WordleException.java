1092f0f44b9abcff9cc2c14afeecfe7cae1177ff
jgajga <gomezarrausi@gmail.com>
2024-04-22 08:32:43 +0200

    WordleException class

---
+++ src/main/java/com/jga/swordle/WordleException.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jga.swordle;
+
+
+public class WordleException extends Exception {
+    private String code;
+
+    public WordleException(String code, String message) {
+        super(message);
+        this.setCode(code);
+    }
+
+    public WordleException(String code, String message, Throwable cause) {
+        super(message, cause);
+        this.setCode(code);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
