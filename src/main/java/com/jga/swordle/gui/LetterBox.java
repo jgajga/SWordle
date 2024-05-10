325b0e3b32b595bc3e139bd12d55f1e2db655195
jgajga <gomezarrausi@gmail.com>
2024-05-10 09:43:24 +0200

    own drawing logic to LetterBox
    Windows Theme does not change backgroud color for buttons

--- src/main/java/com/jga/swordle/gui/LetterBox.java
+++ src/main/java/com/jga/swordle/gui/LetterBox.java
@@ -4,12 +4,19 @@
  */
 package com.jga.swordle.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 
 /**
  *
@@ -17,12 +24,23 @@ import javax.swing.JButton;
  */
 public class LetterBox extends JButton implements ActionListener{
 
+    JLabel label = new JLabel();
+    private Color bg;
+    
     
     public LetterBox() {
-         this.setBackground(Color.WHITE);
-         addActionListener(this);
-         setMargin(new Insets(0, 0, 0, 0));
-         this.setFont(new Font("Dialog", Font.PLAIN, 30));
+        
+
+
+        label.setOpaque(true);
+
+        setLayout(new BorderLayout());
+        add(label, BorderLayout.CENTER);    
+        addActionListener(this);
+        setMargin(new Insets(0, 0, 0, 0));
+        label.setFont(new Font("Dialog", Font.PLAIN, 30));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        this.setBackground(Color.WHITE);
     }
 
     
@@ -39,5 +57,49 @@ public class LetterBox extends JButton implements ActionListener{
             this.setBackground(Color.YELLOW);    
         }        
     }
+    
+    @Override
+    public void setBackground(Color bg) {
+        this.bg = bg;
+        if(label != null) {
+            label.setBackground(bg);
+        }
+    }
+    
+    @Override
+    public Color getBackground() {
+        return this.bg;
+    }    
+    
+    @Override
+    public void setText(String text) {
+        super.setText(text);
+        if(label != null){
+            label.setText(text);
+        }
+    }    
+
+    @Override
+    public void setForeground(Color fg) {
+        super.setForeground(fg);
+        if(label != null){
+            label.setForeground(fg);
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Rectangle rectangle = getBounds();
+        rectangle.x = 0;
+        rectangle.y = 0;
+        rectangle.width -= rectangle.x * 2;
+        rectangle.height -= rectangle.y * 2;
+
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setColor(bg);
+        g2d.fill(rectangle);
+    }    
     
 }
